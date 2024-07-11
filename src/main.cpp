6846eea1092c19c1a5df31c741f810777cac30b9
Eric Dawe <eric.github@redocrehtona.dev>
2024-07-11 16:47:28 +0100

    Updated

--- src/main.cpp
+++ src/main.cpp
@@ -12,19 +12,25 @@ void gpio_callback(uint gpio, uint32_t event_mask) {
 }
  
 void vInputHandler(void* unused_arg) {
+	int loops = 0;
 
+	for (;;) {
+		printf("Running for %d seconds...\n", loops);
+		loops++;
+		vTaskDelay(1000);
+	}
 }
 
 int main() {
 	stdio_init_all();
 
-	gpio_init(PICO_DEFAULT_LED_PIN);
-	gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
+	
+	gpio_init(11);
+	gpio_set_dir(11, GPIO_IN);
+	gpio_pull_up(11);
 
-	gpio_init(16);
-	gpio_set_dir(16, GPIO_IN);
-
-	gpio_set_irq_enabled_with_callback(16, GPIO_IRQ_EDGE_FALL, true, &gpio_callback);
+	gpio_set_irq_enabled_with_callback(11, GPIO_IRQ_EDGE_FALL, true, &gpio_callback);
+	
 
 	xTaskCreate(vInputHandler, "Read rotary encoder", 8192, NULL, 1, NULL);
 
